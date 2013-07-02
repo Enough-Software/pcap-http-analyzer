c655f587a8b33b9a47faf5afcf0a35a894d08d14
Michael Koch <michael.koch@enough.de>
2013-07-02 07:52:37 +0200

    Move ip and tcp structs to own header file

--- src/main.cc
+++ src/main.cc
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <net/ethernet.h>
-#include <netinet/in.h>
 #include <netinet/tcp.h>
 #include <pcap/pcap.h>
 #include <getopt.h>
@@ -16,56 +15,10 @@
 
 #include "commparty.h"
 #include "print.h"
+#include "tcp.h"
 #include "websocket.h"
 
 using namespace std;
-
-typedef u_int32_t tcp_seq;
-
-struct nread_ip {
-  u_int8_t        ip_vhl;          /* header length, version    */
-#define IP_V(ip)    (((ip)->ip_vhl & 0xf0) >> 4)
-#define IP_HL(ip)   ((ip)->ip_vhl & 0x0f)
-  u_int8_t        ip_tos;          /* type of service           */
-  u_int16_t       ip_len;          /* total length              */
-  u_int16_t       ip_id;           /* identification            */
-  u_int16_t       ip_off;          /* fragment offset field     */
-#define IP_DF 0x4000                 /* dont fragment flag        */
-#define IP_MF 0x2000                 /* more fragments flag       */
-#define IP_OFFMASK 0x1fff            /* mask for fragmenting bits */
-  u_int8_t        ip_ttl;          /* time to live              */
-  u_int8_t        ip_p;            /* protocol                  */
-  u_int16_t       ip_sum;          /* checksum                  */
-  struct  in_addr ip_src;
-  struct  in_addr ip_dst;  /* source and dest address   */
-};
-
-struct nread_tcp {
-  u_short th_sport; /* source port            */
-  u_short th_dport; /* destination port       */
-  tcp_seq th_seq;   /* sequence number        */
-  tcp_seq th_ack;   /* acknowledgement number */
-#if BYTE_ORDER == LITTLE_ENDIAN
-  u_int th_x2:4,    /* (unused)    */
-  th_off:4;         /* data offset */
-#endif
-#if BYTE_ORDER == BIG_ENDIAN
-  u_int th_off:4,   /* data offset */
-  th_x2:4;          /* (unused)    */
-#endif
-  u_char th_flags;
-#define TH_FIN 0x01
-#define TH_SYN 0x02
-#define TH_RST 0x04
-#define TH_PUSH 0x08
-#define TH_ACK 0x10
-#define TH_URG 0x20
-#define TH_ECE 0x40
-#define TH_CWR 0x80
-  u_short th_win; /* window */
-  u_short th_sum; /* checksum */
-  u_short th_urp; /* urgent pointer */
-};
 
 static int flag_short = 0;
 static int flag_stopwatch = 0;
